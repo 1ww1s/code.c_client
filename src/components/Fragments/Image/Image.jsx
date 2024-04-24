3a97f49494e2d068f6d93901c5ea6eb7ed9003e0
1ww1s <100320174+1ww1s@users.noreply.github.com>
2024-04-24 18:16:36 +0300

    changed: image behavior for <450 px. The width becomes 100%. For >450px, there will be a scroll bar if the image goes beyond the screen size; createFragment has been improved

--- src/components/Fragments/Image/Image.jsx
+++ src/components/Fragments/Image/Image.jsx
@@ -5,7 +5,7 @@ import classes from './image.module.css'
 const Image = function({fragment, styleImg, ...props}){
 
     return (
-        <div {...props} >
+        <div className={classes.container} {...props} >
             <h2 id={fragment.title}>{fragment.title}</h2>
             <img style={styleImg} className={classes.img} src={fragment.text}></img>
         </div>
